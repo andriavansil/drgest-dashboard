4295c48b4de0482de643763ff43e7a3350d8f2f0
Ândria da Silva <andriavansil@gmail.com>
2026-03-24 17:47:26 +0000

    Refactor Navbar component to accept toggleMenu prop

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,19 @@
-import {Bell, PanelLeftClose} from "lucide-react";
+import { Bell, PanelLeftClose } from "lucide-react";
 import Image from "next/image";
 
-const Navbar = () => {
+const Navbar = ({ toggleMenu }: { toggleMenu: () => void }) => {
   return (
-    <div className='flex items-center justify-between p-4'>
-      {/* SEARCH BAR */}
-      <div className='lg:hidden bg-white rounded-full w-8 h-8 flex items-center justify-center cursor-pointer'>
-        <PanelLeftClose color="grey" size={20}/>
+    <div className="flex items-center justify-between p-4">
+      {/* BUTTON */}
+      <div
+        onClick={toggleMenu}
+        className="lg:hidden bg-white rounded-full w-8 h-8 flex items-center justify-center cursor-pointer"
+      >
+        <PanelLeftClose color="grey" size={20} />
       </div>
-      {/* ICONS AND USER */}
-      <div className='flex items-center gap-6 justify-end w-full'>
+
+      {/* RIGHT SIDE */}
+      <div className="flex items-center gap-6 justify-end w-full">
         {/* DARK MODE TOGGLE 
         <div className='bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer'>
           <Image src="/message.png" alt="" width={20} height={20}/>
@@ -18,14 +22,22 @@ const Navbar = () => {
           <Bell color="grey" size={20}/>
           <div className='absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500 text-white rounded-full text-xs'>1</div>
         </div>*/}
-        <div className='flex flex-col'>
+        <div className="flex flex-col">
           <span className="text-xs leading-3 font-medium">John Doe</span>
-          <span className="text-[10px] text-gray-500 text-right">Cardiologista</span>
+          <span className="text-[10px] text-gray-500 text-right">
+            Cardiologista
+          </span>
         </div>
-        <Image src="/avatar.png" alt="" width={36} height={36} className="rounded-full"/>
+        <Image
+          src="/avatar.png"
+          alt=""
+          width={36}
+          height={36}
+          className="rounded-full"
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
