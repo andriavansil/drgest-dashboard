16124b23325732a0272af33eba398ed6094a3673
Ândria da Silva <andriavansil@gmail.com>
2026-03-24 17:45:42 +0000

    Update DashboardLayout to toggle menu visibility
    Refactor layout to conditionally render the menu based on state.

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import Menu from "@/components/Menu";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
@@ -5,25 +8,34 @@ import Link from "next/link";
 
 export default function DashboardLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
+  const [menuOpen, setMenuOpen] = useState(true);
+
   return (
     <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
-        <Link
-          href="/"
-          className="flex items-center justify-center lg:justify-start gap-2"
-        >
-          <Image src="/logo.png" alt="logo" width={32} height={32} />
-          <span className="hidden lg:block font-bold">Dr. Gest</span>
-        </Link>
-        <Menu />
-      </div>
+      {menuOpen && (
+        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+          <Link
+            href="/"
+            className="flex items-center justify-center lg:justify-start gap-2"
+          >
+            <Image src="/logo.png" alt="logo" width={32} height={32} />
+            <span className="hidden lg:block font-bold">Dr. Gest</span>
+          </Link>
+          <Menu />
+        </div>
+      )}
+
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-y-scroll flex flex-col">
-        <Navbar />
+      <div
+        className={`${
+          menuOpen ? "w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]" : "w-full"
+        } bg-[#F7F8FA] overflow-y-scroll flex flex-col`}
+      >
+        <Navbar toggleMenu={() => setMenuOpen(!menuOpen)} />
         {children}
       </div>
     </div>
